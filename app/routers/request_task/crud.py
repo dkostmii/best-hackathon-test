6e91e3a9204fe40d48e80f6e6c8f0a93e5939051
nikita provorov <nikiyorkovich@gmail.com>
2024-04-24 16:48:18 +0300

    Imlement RequestTask and Priority Schemas

--- app/routers/request_task/crud.py
+++ app/routers/request_task/crud.py
@@ -1,9 +1,31 @@
+from uuid import UUID
+
 from sqlalchemy.orm import Session
 
 from app.routers.request_task.model import RequestTask
+from app.routers.request_task.schema import RequestTaskCreateSchema
+from app.routers.user.model import User
 
 
 class RequestTaskCRUD:
     @staticmethod
-    def get_request_tasks(db: Session):
-        return db.query(RequestTask).all()
+    def get_request_tasks(db: Session, page: int, limit: int):
+        offset = (page - 1) * limit
+        return db.query(RequestTask).offset(offset).limit(limit).all()
+
+    @staticmethod
+    def get_request_task_by_id(pk: UUID, db: Session):
+        return db.query(RequestTask).filter_by(id=pk).first()
+
+    @staticmethod
+    def create_request_task(data: RequestTaskCreateSchema, creator: User, db: Session):
+        request_task = RequestTask(
+            name=data.name,
+            description=data.description,
+            creator_id=creator.id,
+            priority_id=data.priority_id,
+        )
+        db.add(request_task)
+        db.commit()
+        db.refresh(request_task)
+        return request_task
