cf05c11f10c0295ca522ac113e63dd4790a95c72
nikita provorov <nikiyorkovich@gmail.com>
2024-04-24 20:23:38 +0300

    Update users crud

--- app/routers/user/crud.py
+++ app/routers/user/crud.py
@@ -1,24 +1,32 @@
 import secrets
+from uuid import UUID
 
 from sqlalchemy.orm import Session
+from fastapi import Request
+from starlette.templating import Jinja2Templates
 
 from app.routers.user.hashing import Hasher
 from app.routers.user.model import User, Session as SessionModel
-from app.routers.user.schema import UserRegistrationSchema
+from app.routers.user.schema import UserLoginSchema, UserRegistrationSchema
 
 
 class UserCRUD:
+    @staticmethod
+    def get_user_by_id(pk: UUID, db: Session):
+        return db.query(User).filter_by(id=pk).first()
+
     @staticmethod
     def get_users(db: Session):
         return db.query(User).all()
 
     @staticmethod
-    def get_user_by_username(db: Session, username: str):
+    def get_user_by_username(db: Session, username: str) -> User | None:
         return db.query(User).filter_by(username=username).first()
 
     @classmethod
     def create_user(cls, db: Session, data: UserRegistrationSchema):
         validation = cls.validate_user_data(db, data)
+
         if not validation:
             return False
 
@@ -35,9 +43,40 @@ class UserCRUD:
     @classmethod
     def validate_user_data(cls, db: Session, data: UserRegistrationSchema):
         user = cls.get_user_by_username(db, data.username)
+
         if user:
             return False
+
         return True
+
+    @staticmethod
+    def authenticate_user(data: UserLoginSchema, db: Session) -> User | bool:
+        user = UserCRUD.get_user_by_username(db, data.username)
+
+        if not user:
+            return False
+
+        if not Hasher.verify_password(data.password, user.password):
+            return False
+
+        return user
+
+    @staticmethod
+    def login_user_html(
+            request: Request,
+            user: User,
+            db: Session,
+            templates: Jinja2Templates,
+            response_page: str = "base.html"
+    ):
+        session_token = SessionCRUD.create_session_token(user, db)
+        response = templates.TemplateResponse(
+            response_page,
+            {"request": request}
+        )
+        response.set_cookie(key="session_id", value=session_token)
+
+        return response
 
 
 class SessionCRUD:
