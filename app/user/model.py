c0a3addd92f5dc4f64b11b55b2364f5d782b0cfd
nikita provorov <nikiyorkovich@gmail.com>
2024-04-24 13:17:02 +0300

    Implement User model

---
+++ app/user/model.py
@@ -0,0 +1,16 @@
+import uuid
+
+from sqlalchemy import Column, String, DateTime, Boolean, text, UUID
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "base_users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String(255), nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    date_joined = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"))
+    is_staff = Column(Boolean, default=False)
+    is_deleted = Column(Boolean, default=False)
