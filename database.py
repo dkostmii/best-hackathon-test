fd28571aa3aaa1c558859f80234f08b2a11009c1
nikita provorov <nikiyorkovich@gmail.com>
2024-04-24 12:48:45 +0300

    Implement sqlalchemy

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from settings import DB_URL
+
+
+engine = create_engine(
+    DB_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    }
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
