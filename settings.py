499814ce0a156b2060d960fdcba71559a06858a8
nikita provorov <nikiyorkovich@gmail.com>
2024-04-24 13:05:44 +0300

    Update settings

--- settings.py
+++ settings.py
@@ -3,5 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DATABASE = {
+    'NAME': os.environ.get('POSTGRES_DB') or 'postgres_local',
+    'USER': os.environ.get("POSTGRES_USER") or 'postgres',
+    'PASSWORD': os.environ.get("POSTGRES_PASSWORD") or 'postgres',
+    'HOST': os.environ.get('PG_HOST') or 'db',
+    'PORT': str(os.environ.get('PG_PORT', 5432)),
+}
 
-DB_URL = os.environ.get('POSTGRES_DB')
+DB_URL = (
+    f"postgresql://{DATABASE['USER']}"
+    f":{DATABASE['PASSWORD']}"
+    f"@{DATABASE['HOST']}"
+    f":{DATABASE['PORT']}"
+    f"/{DATABASE['NAME']}"
+)
