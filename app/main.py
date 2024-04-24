f0f5e86e090e457f0fb209ee30dad04ebea2edde
nikita provorov <nikiyorkovich@gmail.com>
2024-04-24 19:12:10 +0300

    move templates

--- app/main.py
+++ app/main.py
@@ -1,20 +1,7 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 
 from app.routers import app_router
 
 
 base_app = FastAPI()
 base_app.include_router(app_router)
-
-templates = Jinja2Templates(directory="templates")
-
-
-@base_app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse(
-        "base.html",
-        {
-            "request": request,
-        },
-    )
