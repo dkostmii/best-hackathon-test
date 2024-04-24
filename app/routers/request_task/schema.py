be1c29e7bc14eab7fd43f502554b6dc5fca8db44
nikita provorov <nikiyorkovich@gmail.com>
2024-04-24 16:48:17 +0300

    Imlement RequestTask and Priority Schemas

---
+++ app/routers/request_task/schema.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from app.routers.user.schema import UserSchema
+
+
+class PrioritySchema(BaseModel):
+    id: int
+    name: str
+
+
+class RequestTaskSchema(BaseModel):
+    id: UUID
+    priority: PrioritySchema
+    name: str
+    description: str
+    created_at: datetime
+    creator: UserSchema
+
+
+class RequestTaskCreateSchema(BaseModel):
+    priority_id: str
+    name: str
+    description: str
