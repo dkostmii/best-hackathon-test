7defcb1cdcd39082f9b1a6a5fcdc6d10a0347315
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-04-25 14:47:03 +0300

    Fix: missing priorities
    Temporarily seed db with sample priorities

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,9 +1,26 @@
 from fastapi import APIRouter, Request, Depends
 
-from app.dependencies import templates, get_current_user
+from app.dependencies import templates, get_current_user, get_db
 from app.routers.user.app import user_router
 from app.routers.user.model import User
 from app.routers.request_task.app import request_task_router
+
+from app.routers.request_task.model import Priority
+
+db = next(get_db())
+no_priorities = db.query(Priority).count() == 0
+if no_priorities:
+    priorities = [
+        Priority(name="P1"),
+        Priority(name="P2"),
+        Priority(name="P3")
+    ]
+
+    for priority in priorities:
+        db.add(priority)
+        db.commit()
+        db.refresh(priority)
+
 
 app_router = APIRouter()
 
