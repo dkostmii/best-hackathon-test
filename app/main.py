e34b2c5947bfad45625a6278c4dc490c70a7f4eb
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-04-25 14:38:03 +0300

    Change: exception handlers
    Handle 404 exceptions separately, include status in the template

--- app/main.py
+++ app/main.py
@@ -2,17 +2,30 @@ from fastapi import FastAPI, HTTPException, Request
 
 from app.routers import app_router, templates
 
+
 base_app = FastAPI()
 base_app.include_router(app_router)
 
 
-@base_app.exception_handler(HTTPException)
-async def http_exception_handler(request: Request, exc: Exception):
+@base_app.exception_handler(404)
+async def not_found_handler(request: Request, exc: HTTPException):
     return templates.TemplateResponse(
         name="error.html",
         context={
             "request": request,
-            "error": {"title": "Error occurred", "description": exc.detail},
+            "error": {"title": "Not found", "description": exc.detail, "status": exc.status_code},
+            "no_auth_controls": True,
+        }
+    )
+
+
+@base_app.exception_handler(HTTPException)
+async def server_error_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse(
+        name="error.html",
+        context={
+            "request": request,
+            "error": {"title": "Server error occurred", "description": exc.detail, "status": exc.status_code},
             "no_auth_controls": True,
         }
     )
