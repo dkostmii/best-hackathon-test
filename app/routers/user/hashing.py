8d835b62b0186daff02593937860454046de3e41
nikita provorov <nikiyorkovich@gmail.com>
2024-04-25 13:37:46 +0300

    Delete passlib, use bcrypt

--- app/routers/user/hashing.py
+++ app/routers/user/hashing.py
@@ -1,13 +1,13 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 
 class Hasher:
     @staticmethod
     def verify_password(plain_password, hashed_password):
-        return pwd_context.verify(plain_password, hashed_password)
+        return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
     @staticmethod
     def get_password_hash(password):
-        return pwd_context.hash(password)
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed.decode('utf-8')
